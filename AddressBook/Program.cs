f62833218fc9db8503ce8c50257862f29387f678
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-06-04 23:01:09 +0530

    [jayesh]  Create Contact details in AddressBook

---
+++ AddressBook/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AddressBook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ContactDetails contactDetails = new ContactDetails();
+            contactDetails.Contact();
+        }
+
+    }
+
+}
