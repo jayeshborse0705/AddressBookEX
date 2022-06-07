b7841bb6e599835cf6846cb2735048425a5c8acb
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-06-07 20:22:52 +0530

    [jayesh]  Add Multiple contact in Address book

---
+++ AddressBook/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Contact
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public string Email;
+        public int Zip;
+        public long PhoneNumber;
+        public Contact (string FirstName, string LastName, string Address, string City, string State, string Email, int Zip, long PhoneNumber)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Address = Address;
+            this.City = City;
+            this.State = State;
+            this.Zip = Zip;
+            this.PhoneNumber = PhoneNumber;
+        }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,8 +4,95 @@
     {
         public static void Main(string[] args)
         {
-            ContactDetails contactDetails = new ContactDetails();
-            contactDetails.AddMultipleContact();
+            while (true)
+            {
+                Console.WriteLine("WelCome To The Address Book");
+                Console.WriteLine("Select The Option For The Particular Address Book");
+                Console.WriteLine("1 for the Friends.");
+                Console.WriteLine("2 for the Office.");
+                int option = Convert.ToInt32(Console.ReadLine());
+                Program details = new Program();
+                switch (option)
+                {
+                    case 1:
+                        Friends friend = new Friends();
+                        do
+                        {
+                            Console.WriteLine("\n1:To Add the Contacts");
+                            Console.WriteLine("2:To View the contacts");
+                            Console.WriteLine("3:To Edit the Contacts");
+                            Console.WriteLine("4:To Remove a contact");
+                            try
+                            {
+                                int option1 = Convert.ToInt32(Console.ReadLine());
+                                switch (option1)
+                                {
+                                    case 1:
+                                        friend.AddingContacts();
+                                        continue;
+                                    case 2:
+                                        friend.DisplayContacts();
+                                        break;
+                                    case 3:
+                                        friend.EditContact();
+                                        break;
+                                    case 4:
+                                        friend.DeleteContact();
+                                        break;
+                                    case 0:
+                                        Console.WriteLine("Exit");
+                                        break;
+                                    default:
+                                        Console.WriteLine("Enter a valid Input");
+                                        break;
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("Enter a proper Value");
+                            }
+                        }
+                        while (option != 0);
+                        break;
+                    case 2:
+                        Office office = new Office();
+                        do
+                        {
+                            Console.WriteLine("\n1:To Add the Contacts");
+                            Console.WriteLine("2:To View the contacts");
+                            Console.WriteLine("3:To Edit the Contacts");
+                            Console.WriteLine("4:To Remove a contact");
+                            try
+                            {
+                                int option1 = Convert.ToInt32(Console.ReadLine());
+                                switch (option1)
+                                {
+                                    case 1:
+                                        office.AddingContacts();
+                                        continue;
+                                    case 2:
+                                        office.DisplayContacts();
+                                        break;
+                                    case 3:
+                                        office.EditContact();
+                                        break;
+                                    case 4:
+                                        office.DeleteContact();
+                                        break;
+                                    default:
+                                        Console.WriteLine("Enter a valid Input");
+                                        break;
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("Enter a proper Value");
+                            }
+                        }
+                        while (option != 0);
+                        break;
+                }
+            }
         }
 
     }
